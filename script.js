3f862c30e7741b863700fc8bce02b4d84bf13295
Arthur Schmidt Dias Granito <163346646+arthurgranito@users.noreply.github.com>
2025-05-12 08:46:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const botaoAbrir = document.getElementById('botao-abrir');
-const botaoFechar = document.getElementById('botao-fechar');
+const openButton = document.getElementById('open-button');
+const closeButton = document.getElementById('close-button');
 const body = document.getElementById('body');
 const nav = document.getElementById('nav');
 
-botaoAbrir.addEventListener('click', () => {
+openButton.addEventListener('click', () => {
     nav.classList.remove('fechar');
     nav.classList.add('abrir');
     body.classList.add('escurecer');
 });
 
-botaoFechar.addEventListener('click', () => {
+closeButton.addEventListener('click', () => {
     nav.classList.remove('abrir');
     nav.classList.add('fechar');
     body.classList.remove('escurecer');
